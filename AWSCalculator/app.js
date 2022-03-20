3bca638a7ff05d017c1ec5ed0ec436ca73c51523
Danny Renato <danny@campaignagent.com.au>
2022-03-20 20:15:39 +1100

    init

---
+++ AWSCalculator/app.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
+
+export class CalculatorService {}
