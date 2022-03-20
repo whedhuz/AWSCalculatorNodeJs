4ab7f30ce5be09bedbf1955e4cd3df3d2b8d5c6c
Danny Renato <danny@campaignagent.com.au>
2022-03-20 21:06:03 +1100

    add calculator to calculator service

--- AWSCalculator/calculator-service.js
+++ AWSCalculator/calculator-service.js
@@ -1,12 +1,31 @@
+const Calculator = require("./calculator");
+
 module.exports = class CalculatorService {
   validOperations = ["ADD"];
+  #calculator;
+
+  constructor() {
+    this.#calculator = new Calculator();
+  }
 
   calculate(inputs) {
     if (!this.isValid(inputs)) {
       return { error: "Invalid input parameter", result: null };
     }
 
-    return { error: null, result: 3 };
+    let number1 = inputs.number1;
+    let number2 = inputs.number2;
+    let result = 0;
+
+    switch (inputs.operation) {
+      case "ADD":
+        result = this.#calculator.add(number1, number2);
+        break;
+      default:
+        return { error: "Invalid operation", result: null };
+    }
+
+    return { error: null, result: result };
   }
 
   // Check if the inputs contain expected data structure and the given operation is in the expected list of operations
